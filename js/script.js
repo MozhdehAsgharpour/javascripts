cb3d5e8cde5adbd6ebf9f1d6d151a66dfd6796f1
mozhdeh asgharpour <mozhdeh.asgharpour121367@outlook.com>
2021-09-20 21:13:22 +0430

    added videoSlider

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ function toggleMenu(){
   const tglMenu = document.querySelector('.menu');
   tglMenu.classList.toggle('active'); 
 }
+function getUrl(url){
+  document.getElementById('slider').src = "video/" + url; 
+}
