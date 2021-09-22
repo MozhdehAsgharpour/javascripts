4c46c9c3079e6fa1b58feffbd6509986f705f526
mozhdeh asgharpour <mozhdeh.asgharpour121367@outlook.com>
2021-09-23 00:44:24 +0330

    added 404 an slider

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ function toggleMenu(){
 function getUrl(url){
   document.getElementById('slider').src = "video/" + url; 
 }
+const  containar = document.querySelector('#containar');
+window.onmousemove = function(e){
+  let x = e.clientX / 5;
+  let y = e.clientY / 5;
+
+  containar.style.backgroundPositionX = x + "px";
+  containar.style.backgroundPositionY = y + "px";
+}
